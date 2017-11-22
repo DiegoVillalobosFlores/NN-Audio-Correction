e9f46d228552881840b2e26f2549b9704dc96893
deimi <diego@iotekholding.com>
2017-11-22 02:03:59 -0600

    Added neural network training

--- AudioCorrection.m
+++ AudioCorrection.m
@@ -1,3 +1,5 @@
+clear;
+close all;
 [y,fs] = audioread('Grandma.mp3');
 cR = y(:,1);
 cL = y(:,2);
@@ -5,11 +7,14 @@ cL = y(:,2);
 n = length(y);
 
 t = linspace(0, n/fs, n);
+target = y;
 
 pN = 30;
 
-nY = y;
-for cont = 10:n
+yParsed = y(690194:690220) * 10;
+
+nY = yParsed;
+for cont = 1:length(nY)
     r = 100.*rand(1,1) + 1;
     if(r <= pN)
         nY(cont,1) = 0;
@@ -17,7 +22,20 @@ for cont = 10:n
     end
 end
 
+tln1 = 1:length(nY);
 gnY = awgn(y,1,'measured');
+
+net = feedforwardnet([10 10]);
+net = configure(net,nY,yParsed);
+figure();
+plot(tln1,yParsed(:,2),'o');
+
+ny1 = net(nY);
+
+net = train(net,nY,yParsed);
+ny2 = net(nY);
+figure();
+plot(tln1,yParsed(:,2),'o',tln1,ny1(:,2),'x',tln1,ny2(:,2),'*');
 
 figure();
 
@@ -34,7 +52,7 @@ ylabel('Amplitud');
 title("Canal Izquierdo");
 
 subplot(3,1,3);
-plot(t,nY);
+plot(tln1,nY);
 xlabel('Segundos');
 ylabel('Amplitud');
 title("Señal con Ruido");
